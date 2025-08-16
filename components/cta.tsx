14c276f2f9581777696407d71174d4fe5f7bd265
mikaelpackowski <mikael.packowski@gmail.com>
2025-08-16 10:07:01 +0200

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -7,7 +7,7 @@ export default function Cta() {
       {/* Arrière-plan flou */}
       <div
         className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -mb-24 ml-20 -translate-x-1/2"
-        aria-hidden="true"
+        aria-hidden="true" 
       >
         <Image
           className="max-w-none"
